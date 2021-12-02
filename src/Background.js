23a2a2c9ad2e3fb7b85ac8613c140c4606e47c00
spatel912020 <spatel91@vols.utk.edu>
2021-12-02 16:26:43 -0500

    try 6

--- src/Background.js
+++ src/Background.js
@@ -3,7 +3,7 @@ import './Background.css';
 function Background() {
   return (
     <div className="background">
-      <img className = "background_img" src="https://media-exp1.licdn.com/dms/image/C4E03AQGeFDVJ21lR0A/profile-displayphoto-shrink_400_400/0/1638477954334?e=1643846400&v=beta&t=BnRAHtZTEPAFarw8Q9zHNllgLDtadintZVXaIf_iwVs" />
+      <img className = "background_img" src="" />
       <div className = "background_header">
         <h1 className = "background_header_text">Shreyank Patel</h1>
         <h1 className = "background_subheader_text" >Address: 112 Wolf Creek Way, Oak Ridge, TN</h1>
